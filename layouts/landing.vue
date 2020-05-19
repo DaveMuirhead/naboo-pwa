f8c9a30bb01341cac71ffe8d7b6c38511c2e1d61
Dave Muirhead <dave@brsg.io>
2020-05-19 14:34:10 -0400

    Adjusted to simple landing page layout

---
+++ layouts/landing.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-img :src="require('@/assets/images/icon.png')" width="300" height="120"></v-img>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col md="8" sm="12" >
+            <nuxt />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false
+    };
+  }
+};
+</script>
