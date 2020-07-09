895ad10cbb11ef3f4e2782c2ccb8e1782a9e0bae
Dave Muirhead <dave@brsg.io>
2020-07-08 22:06:20 -0400

    sync error message approach with registration

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -6,19 +6,15 @@
           <v-card>
             <v-card-title class="headline">Log into UpSprout</v-card-title>
             <v-card-text>
+
               <!-- Email -->
               <v-text-field
                 v-model="email"
                 label="Email Address"
                 prepend-icon="mdi-email"
                 @blur="$v.email.$touch"
+                :error-messages="emailErrors"
               ></v-text-field>
-              <v-alert
-                dense
-                outlined
-                type="error"
-                v-if="$v.email.$dirty && (!$v.email.required || !$v.email.email)"
-              >Please enter your email address in format: yourname@example.com</v-alert>
 
               <!-- Password -->
               <v-text-field
@@ -29,17 +25,13 @@
                 :type="showPassword ? 'text' : 'password'"
                 :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
                 @click:append="showPassword = !showPassword"
+                :error-messages="passwordErrors"
               ></v-text-field>
-              <v-alert
-                dense
-                outlined
-                type="error"
-                v-if="$v.password.$dirty && !$v.password.required"
-              >Please enter your password.</v-alert>
+
             </v-card-text>
             <v-card-actions>
               <v-spacer />
-              <v-btn type="submit" color="primary" :disabled="!isFormValid">Log In</v-btn>
+              <v-btn type="submit" color="primary">Log In</v-btn>
               <v-spacer />
             </v-card-actions>
             <v-card-text>
@@ -52,17 +44,11 @@
                 <NuxtLink to="/onboarding">Get Started</NuxtLink>
               </div>
             </v-card-text>
+            <v-snackbar v-model="snackbar" :timeout="timeout" color="red" top>
+              Invalid email or password. Please try again.
+            </v-snackbar>
           </v-card>
         </v-form>
-      </v-col>
-    </v-row>
-    <v-row justify="center">
-      <v-col md="4" sm="6">
-        <v-snackbar
-          v-model="snackbar"
-          :timeout="timeout"
-          color="red"
-        >Invalid email or password. Please try again.</v-snackbar>
       </v-col>
     </v-row>
   </v-container>
@@ -92,31 +78,49 @@ export default {
   },
   methods: {
     login() {
-      this.$auth
-        .loginWith("local", {
-          data: {
-            email: this.email,
-            password: this.password
-          } 
-        })
-        .then(result => {
-          console.log("login succeeded with result " + result);
-        })
-        .catch(errors => {
-          console.log("login failed with errors " + errors);
-          this.snackbar = true;
-        });
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        this.$auth
+          .loginWith("local", {
+            data: {
+              email: this.email,
+              password: this.password
+            } 
+          })
+          .then(result => {
+            console.log("login succeeded with result " + result);
+          })
+          .catch(errors => {
+            console.log("login failed with errors " + errors);
+            this.snackbar = true;
+          });
+      }
     }
   },
   computed: {
-    isFormValid() {
-      this.$v.touch
-      var result =
-        this.$v.email.$dirty &&
-        this.$v.password.$dirty &&
-        !this.$v.$anyError
-      return result
+    emailErrors() {
+      const errors = []
+      if (!this.$v.email.$dirty) {
+        return errors;
+      }
+      else if (!this.$v.email.required) {
+        errors.push('Email is required.')
+      }
+      else if (!this.$v.email.email) {
+        errors.push('Email must be in the format you@yourco.com')
+      }
+      return errors;
     },
+    passwordErrors() {
+      const errors = []
+      if (!this.$v.password.$dirty) {
+        return errors;
+      }
+      else if (!this.$v.password.required) {
+        errors.push('Password is required.')
+      }
+      return errors;
+    }
   }
 };
 </script>
